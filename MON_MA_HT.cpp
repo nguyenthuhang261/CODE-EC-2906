881f9e59d09499cdbd19ca5b223c4c0c0c9fbb02
nguyenthuhang261 <nthnguyenthuhang2610@gmail.com>
2024-06-30 07:01:10 +0700

    Update MON_MA_HT.cpp

--- MON_MA_HT.cpp
+++ MON_MA_HT.cpp
@@ -1,25 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 
-// em chua nghi ra cach sap xep khi no la ky tu
-
-// sap xep mon khi la so
 struct mon{
-	int ma;
-	int mon;
-	int ht;
+	char ma[500];
+	char mon[500];
+	char ht[500];
 };
-struct mon k, p[1005];
+struct mon p[5];
 
 int main()
 {
 	for(int i = 1; i <= 3; i++){
-		scanf("%d%d%d", &p[i].ma, &p[i].mon, &p[i].ht);
-		
+		scanf("%s%s%s", p[i].ma, p[i].mon, p[i].ht);
 	}
-	for(int i = 1; i <= 2; i++){
+	for(int i = 1; i <= 3; i++){
 		for(int j = i; j <= 3; j++){
-			if(p[i].ma > p[j].ma){
+			if(strcmp(p[i].ma, p[j].ma) > 0){
+				struct mon k;
 				k = p[i];
 				p[i] = p[j];
 				p[j] = k;
@@ -27,7 +24,6 @@ int main()
 		}
 	}
 	for(int i = 1; i <= 3; i++){
-		printf("%d %d %d\n", p[i].ma, p[i].mon, p[i].ht);
+		printf("%s %s %s\n", p[i].ma, p[i].mon, p[i].ht);
 	}
 }
-
